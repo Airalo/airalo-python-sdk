f2d71a78fb5d63b788fe2fd7b7a9abfcb1b6be80
MiroIsikiyski <miroslav.isikiyski@airalo.com>
2025-08-12 16:36:23 +0300

    add topupservice in __init__.py

--- airalo/services/__init__.py
+++ airalo/services/__init__.py
@@ -5,5 +5,6 @@ Service classes for Airalo SDK.
 from .oauth_service import OAuthService
 from .packages_service import PackagesService
 from .order_service import OrderService
+from .topup_service import TopupService
 
-__all__ = ["OAuthService", "PackagesService", "OrderService"]
+__all__ = ["OAuthService", "PackagesService", "OrderService", "TopupService"]

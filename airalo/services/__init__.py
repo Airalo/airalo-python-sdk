07e070c79c7cfa073d7ff278a87ef03c4dfcae00
kminanov <kiril.minanov@airalo.com>
2025-08-13 09:40:28 +0200

    feat: Future order service (AP-6733)

--- airalo/services/__init__.py
+++ airalo/services/__init__.py
@@ -6,6 +6,6 @@ from .oauth_service import OAuthService
 from .packages_service import PackagesService
 from .order_service import OrderService
 from .installation_instructions_service import InstallationInstructionsService
-from .topup_service.py import TopupService
+from .topup_service import TopupService
 
 __all__ = ["OAuthService", "PackagesService", "OrderService", "TopupService", "InstallationInstructionsService"]
